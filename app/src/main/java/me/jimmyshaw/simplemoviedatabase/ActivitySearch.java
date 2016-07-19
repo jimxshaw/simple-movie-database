e501976081f93ac4ac1b14288f0cfd10078f6133
Jim Shaw <jimxshaw@gmail.com>
2016-07-19 17:18:35 -0500

    initial commit

---
+++ app/src/main/java/me/jimmyshaw/simplemoviedatabase/ActivitySearch.java
@@ -0,0 +1,13 @@
+package me.jimmyshaw.simplemoviedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
