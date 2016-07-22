604efec4c2f98f89783984467ec77ee8adbe1452
Jim Shaw <jimxshaw@gmail.com>
2016-07-21 19:33:56 -0500

    Added empty movies and toolbar layouts and assets folder.

--- app/src/main/java/me/jimmyshaw/simplemoviedatabase/ActivityMain.java
+++ app/src/main/java/me/jimmyshaw/simplemoviedatabase/ActivityMain.java
@@ -1,13 +1,13 @@
 package me.jimmyshaw.simplemoviedatabase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class ActivitySearch extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_main);
     }
 }
