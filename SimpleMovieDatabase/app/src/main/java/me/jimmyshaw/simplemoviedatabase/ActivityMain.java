7fe4b752bc9603b6567766945aa9ada2d041d7a9
Jim Shaw <jimxshaw@gmail.com>
2016-07-22 11:40:54 -0500

    Created boilerplate dialog search class. Modified dialog search layout.

--- SimpleMovieDatabase/app/src/main/java/me/jimmyshaw/simplemoviedatabase/ActivityMain.java
+++ SimpleMovieDatabase/app/src/main/java/me/jimmyshaw/simplemoviedatabase/ActivityMain.java
@@ -2,12 +2,42 @@ package me.jimmyshaw.simplemoviedatabase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ActivityMain extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.button_search)
+    Button buttonSearch;
+
+    private View.OnClickListener searchListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            showDialogSearch();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        buttonSearch.setOnClickListener(searchListener);
+    }
+
+    private void showDialogSearch() {
+        DialogSearch dialog = new DialogSearch();
+        dialog.show(getSupportFragmentManager(), "Dialog Search");
     }
 }
---
+++ SimpleMovieDatabase/app/src/main/java/me/jimmyshaw/simplemoviedatabase/DialogSearch.java
@@ -0,0 +1,29 @@
+package me.jimmyshaw.simplemoviedatabase;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogSearch extends DialogFragment {
+
+    public DialogSearch() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_search, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
